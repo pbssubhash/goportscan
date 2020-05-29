20b3a0e9fe9ce5e947ba61deb4186b19c3d27bb7
p1k4chu <pbssubhash@gmail.com>
2020-05-29 17:02:06 +0530

    Com

--- pkg/core_scanner.go
+++ pkg/core_scanner.go
@@ -9,27 +9,36 @@ type result struct {
 	port string
 	status string
 }
-func scan(ip string, port string, scantype string)result{
-	var status string
+
+func scan(ip string, port string, scantype string) result {
 	conn, err := net.Dial(scantype, ip+":"+port)
 	if err!=nil{
-		status = "Closed"
-		return result{port,status}
+		return result{port,"Closed"}
 	}
 	defer conn.Close()
-	status = "Open"
-	return result{port, status}
+	return result{port,"Open"}
 }
-
-func scanTop(ip string) [] result{
+func scanTop(ip string) []result {
 	var finalres []result
-	for i:=79;i<=181;i++{
-		resultx := scan(ip,strconv.Itoa(i),"tcp")
-		finalres = append(finalres,resultx)
-		fmt.Printf("%v \n",resultx)
+	var ch = make(chan result)
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			results := scan(ip, strconv.Itoa(i), "tcp")
+			fmt.Printf("%v \n", results)
+			ch <- results
+			finalres = append(finalres, results)
+		}
+		ch <- result{status:"q",port:"q"}
+	}()
+	for res := range ch {
+		finalres := append(finalres, res)
+		if res.status == "q" {
+			return finalres
+		}
 	}
 	return finalres
 }
+
 func main(){
 	fmt.Println(scanTop("192.168.1.1"))
 }
